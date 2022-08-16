f4017a0d6acdbf1709c5a8c3adc245a1528bb992
karellehofler <karellehofler@gmail.com>
2022-08-16 11:12:30 -0400

    Init commit

---
+++ src/components/Appbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Appbar() {
+    return (
+        <nav>
+            <div>
+                <h1>Notes</h1>
+            </div>
+            <div>
+                <button>Create New Notes</button>
+            </div>
+        </nav>
+    )
+}
